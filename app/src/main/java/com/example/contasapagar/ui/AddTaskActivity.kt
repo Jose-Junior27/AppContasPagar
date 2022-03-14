fab588c4ab9944bff73c2b3e7807b83a9c435872
Jose-Junior27 <junior.jm.sj25@gmail.com>
2022-03-13 22:16:34 -0300

    Atualização funções de Edição

--- app/src/main/java/com/example/contasapagar/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/contasapagar/ui/AddTaskActivity.kt
@@ -1,11 +1,17 @@
 package com.example.contasapagar.ui
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.contasapagar.databinding.ActivityAddTaskBinding
+import com.example.contasapagar.datasource.TaskDataSource
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.example.contasapagar.extensions.format
 import com.example.contasapagar.extensions.text
+import com.example.contasapagar.model.Task
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
 class AddTaskActivity: AppCompatActivity() {
@@ -17,6 +23,14 @@ class AddTaskActivity: AppCompatActivity() {
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let {
+                binding.tilTitle.text = it.title
+                binding.tilData.text = it.date
+                binding.tilHora.text = it.hour
+            }
+        }
         insertListeners()
 
     }
@@ -32,6 +46,43 @@ class AddTaskActivity: AppCompatActivity() {
 
             datePicker.show(supportFragmentManager,"DATE_PICKER_TAG")
         }
+
+        binding.tilHora.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+            timePicker.addOnPositiveButtonClickListener {
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+
+                binding.tilHora.text = "$hour:$minute"
+            }
+
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        binding.btnCancel.setOnClickListener{
+            finish()
+        }
+
+        binding.btnNewTask.setOnClickListener {
+            val task = Task(
+                title = binding.tilTitle.text,
+                date = binding.tilData.text,
+                hour = binding.tilHora.text,
+                id = intent.getIntExtra(TASK_ID, 0)
+            )
+            TaskDataSource.insertTask(task)
+
+            setResult(Activity.RESULT_OK)
+            finish()
+            //Log.e("TAG", "insertListeners: " + TaskDataSource.getList() )
+        }
+
+    }
+
+    companion object{
+        const val TASK_ID = "task_id"
     }
 
 }
